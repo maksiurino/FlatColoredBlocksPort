efcf976d5ad9dc9ba299971f934cc70a6c569c37
maksiurino <153639807+maksiurino@users.noreply.github.com>
2026-02-15 18:34:24 +0100

    Adding features

--- src/main/java/mod/flatcoloredblocks/fabric/registry/block/type/PaintingMixerBlock.java
+++ src/main/java/mod/flatcoloredblocks/fabric/registry/block/type/PaintingMixerBlock.java
@@ -1,19 +1,36 @@
 package mod.flatcoloredblocks.fabric.registry.block.type;
 
 import com.mojang.serialization.MapCodec;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import org.jspecify.annotations.NonNull;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import org.jspecify.annotations.*;
 
 public class PaintingMixerBlock extends HorizontalDirectionalBlock {
     public static final MapCodec<PaintingMixerBlock> CODEC = simpleCodec(PaintingMixerBlock::new);
 
     public PaintingMixerBlock(Properties properties) {
         super(properties);
+        registerDefaultState(defaultBlockState().setValue(FACING, Direction.NORTH));
     }
 
     @Override
     @NonNull
     public MapCodec<PaintingMixerBlock> codec() {
         return CODEC;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        return defaultBlockState().setValue(FACING, blockPlaceContext.getHorizontalDirection().getOpposite());
     }
 }
