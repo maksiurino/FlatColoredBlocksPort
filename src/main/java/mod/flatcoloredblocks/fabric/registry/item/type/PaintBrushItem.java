43be2fb27cc6ca4e98edbc3f806a3e5fd7795378
maksiurino <153639807+maksiurino@users.noreply.github.com>
2026-02-18 20:50:59 +0100

    Adding features

---
+++ src/main/java/mod/flatcoloredblocks/fabric/registry/item/type/PaintBrushItem.java
@@ -0,0 +1,9 @@
+package mod.flatcoloredblocks.fabric.registry.item.type;
+
+import net.minecraft.world.item.Item;
+
+public class PaintBrushItem extends Item {
+    public PaintBrushItem(Properties properties) {
+        super(properties);
+    }
+}
